2e72ef742d89465e59fbb0502f85e5f90c2b11e0
Boniwayne <boniwayne125@gmail.com>
2023-01-17 21:51:48 +0300

    Added files

---
+++ size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - size of variables
+ *
+ * Return: always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	printf("The size of char in my computer is: %ld\n", sizeof(char));
+	printf("The size of float in my computer is: %ld\n", sizeof(float));
+	printf("The size of int in my computer is : %ld\n", sizeof(int));
+
+	return (0);
+
+
+
+
+
+}
